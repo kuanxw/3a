f4522294e654de527b9360681a349171f066c163
Xiang Wen Kuan <classkua@lnxsrv09.seas.ucla.edu>
2017-11-23 17:28:05 -0800

    I/O file processing

--- Makefile
+++ Makefile
@@ -4,13 +4,13 @@
 
 # NAME: Amir Saad
 # EMAIL: 
-# ID: 
+# ID: 604840359
 
 default:
 	gcc lab3a.c -o lab3a -Wall -Wextra
 clean:
-	rm -f lab3a-004461554-NEWID.tar.gz lab3a
+	rm -f lab3a-004461554-604840359.tar.gz lab3a
 dist:
-	tar -cvzf lab3a-004461554-NEWID.tar.gz lab3a.c Makefile README
+	tar -cvzf lab3a-004461554-604840359.tar.gz lab3a.c Makefile README
 check: dist
-	./P3A_check.sh 004461554 NEWID
+	./P3A_check.sh 004461554 604840359
--- lab3a.c
+++ lab3a.c
@@ -4,15 +4,46 @@
  *
  * NAME: Amir Saad
  * EMAIL: 
- * ID: 
+ * ID: 604840359
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+//Error handling
 extern int errno;
+int errVar;
 
+//Input/Output files
+char* imgFile;
+int imgfd;
+int outfd
+
+//Main: Includes in/out file handling
 int main(int argc, char **argv){
+	if(argc != 2){
+		fprintf(stderr,"Usage: ./lab3c IMGFILE\n");
+		exit(1);
+	}
+	imgFile = argv[1];
+	imgfd = open(imgFile, O_RDONLY);
+	if(imgfd < 0){
+		errVar = errno;
+		fprintf(stderr,"Failed to open input file:  %s\n", strerror(errVar));
+		exit(1);
+	}
+	outfd = creat("out.csv",0666);
+	if(outfd < 0){
+		errVar = errno;
+		fprintf(stderr,"Failed to create output file:  %s\n", strerror(errVar));
+		exit(1);
+	}
+	
+	close(outfd);
+	close(imgfd);
     exit(0);
 }
