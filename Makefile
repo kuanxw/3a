1c9ac395df054d57a54024cab84b45c6f3f246cf
Xiang Wen Kuan <classkua@lnxsrv09.seas.ucla.edu>
2017-11-23 19:12:44 -0800

    Student information done. Blank framework for write_ext2_info

--- Makefile
+++ Makefile
@@ -1,16 +1,12 @@
-# NAME: Kuan Xiang Wen
-# EMAIL: kuanxw@g.ucla.edu
-# ID: 004461554
-
-# NAME: Amir Saad
-# EMAIL: 
-# ID: 604840359
+# NAME: Kuan Xiang Wen, Amir Saad
+# EMAIL: kuanxw@g.ucla.edu, arsaad@g.ucla.edu
+# ID: 004461554, 604840359
 
 default:
 	gcc lab3a.c -o lab3a -Wall -Wextra
 clean:
-	rm -f lab3a-004461554-604840359.tar.gz lab3a
+	rm -f lab3a-004461554-604840359.tar.gz lab3a out.csv
 dist:
-	tar -cvzf lab3a-004461554-604840359.tar.gz lab3a.c Makefile README
+	tar -cvzf lab3a-004461554-604840359.tar.gz lab3a.c Makefile README ext2_fs.h
 check: dist
 	./P3A_check.sh 004461554 604840359
--- lab3a.c
+++ lab3a.c
@@ -1,18 +1,18 @@
-/* NAME: Kuan Xiang Wen
- * EMAIL: kuanxw@g.ucla.edu
- * ID: 004461554
- *
- * NAME: Amir Saad
- * EMAIL: 
- * ID: 604840359
+/* NAME: Kuan Xiang Wen, Amir Saad
+ * EMAIL: kuanxw@g.ucla.edu, arsaad@g.ucla.edu
+ * ID: 004461554, 604840359
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <time.h>
+#include "ext2_fs.h"
 
 //Error handling
 extern int errno;
@@ -21,7 +21,13 @@ int errVar;
 //Input/Output files
 char* imgFile;
 int imgfd;
-int outfd
+int outfd;
+
+//Read all the information into relevant structs, then prints them into CSV format
+void write_ext2_info(){
+	pread();
+	
+}
 
 //Main: Includes in/out file handling
 int main(int argc, char **argv){
@@ -42,6 +48,8 @@ int main(int argc, char **argv){
 		fprintf(stderr,"Failed to create output file:  %s\n", strerror(errVar));
 		exit(1);
 	}
+	
+	write_ext2_info();
 	
 	close(outfd);
 	close(imgfd);
